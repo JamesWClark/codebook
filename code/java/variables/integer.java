8700ad87a81d45d0f552f8050c45f3c4feb70307
JamesWClark <jwclark@rockhursths.edu>
2016-06-12 11:33:36 -0500

    more files

---
+++ code/java/variables/integer.java
@@ -0,0 +1,8 @@
+//declare an integer
+int a;
+
+//initialize the integer
+a = 5;
+
+//declare and initialize an integer
+int b = 4;
--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 var codebook = angular.module("codebook",[]);
 
 codebook.controller('codebookController', function($scope) {
+  
+  $scope.langs = ['c','cpp','csharp','java','javascript','php','python','r','ruby','scala','swift'];
+  $scope.topics = ['variables'];
+  
+  angular.element(document).ready(function() {
+    $('#code-container').attr('ng-include', '/code/java/variables/long.html');
+  });
+  
   $scope.$on('$includeContentLoaded', function(event, target){
     console.log(event);
     console.log(target);
@@ -10,4 +18,5 @@ codebook.controller('codebookController', function($scope) {
     $('.prettyprinted').removeClass('prettyprinted');
     prettyPrint();
   };
+  
 });
